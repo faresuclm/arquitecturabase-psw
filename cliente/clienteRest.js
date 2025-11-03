6975f4e792ce613a098b497efe08d2da8155e085
Fares <faresendongpro@gmail.com>
2025-11-03 10:37:28 +0100

    Pequeños cambios antes del sprint2

--- cliente/clienteRest.js
+++ cliente/clienteRest.js
@@ -1,13 +1,17 @@
 function ClienteRest() {
+  
   this.agregarUsuario = function (nick) {
     var cli = this;
-
     $.getJSON("/agregarUsuario/" + nick, function (data) {
+      let msg = "El nick " + nick + " está ocupado";
       if (data.nick != -1) {
         console.log("Usuario " + nick + " ha sido registrado");
+        msg = "Bienvenido al sistema, " + nick;
+        localStorage.setItem("nick", nick);
       } else {
         console.log("El nick ya está ocupado");
       }
+      cw.mostrarMensaje(msg);
     });
   };
 
--- cliente/controlWeb.js
+++ cliente/controlWeb.js
@@ -1,15 +1,44 @@
 function ControlWeb() {
   
-    this.mostrarAgregarUsuario = function () {
-    let cadena = '<div id="au"class="form-group">';
+  this.mostrarAgregarUsuario = function () {
+    let cadena = '<div id="au" class="form-group">';
     cadena = cadena + '<label for="nick">Name:</label>';
-    cadena = cadena + '<input type="text" class="form-control" id="nick">';
-    cadena = cadena + '<button type="submit" class="btn btn-primary">Submit</button>';
+    cadena = cadena + '<input type="text" class="form-control" id="nick" placeholder="Introduce tu nick">';
+    cadena =
+      cadena +
+      '<button id="btnAU" type="submit" class="btn btn-primary">Submit</button>';
     cadena = cadena + "</div>";
     $("#au").append(cadena);
     $("#btnAU").on("click", function () {
       let nick = $("#nick").val();
       rest.agregarUsuario(nick);
+      cw.eliminarFormulario();
     });
+  };
+
+  this.eliminarFormulario = function () {
+    let cadena = '<div id="au" class="form-group">';
+    cadena = cadena + '<label for="nick">Name:</label>';
+    cadena = cadena + '<input type="text" class="form-control" id="nick">';
+    cadena =
+      cadena +
+      '<button id="btnAU" type="submit" class="btn btn-primary">Submit</button>';
+    cadena = cadena + "</div>";
+    $("#btnAU").on("click", function () {
+      $("#au").remove();
+    });
+  };
+
+  this.comprobarSesion = function () {
+    let nick = localStorage.getItem("nick");
+    if (nick) {
+      cw.mostrarMensaje("Bienvenido al sistema, " + nick);
+    } else {
+      cw.mostrarAgregarUsuario();
+    }
+  };
+
+  this.mostrarMensaje = function (msg) {
+    $("#mensaje").html(msg);
   };
 }
